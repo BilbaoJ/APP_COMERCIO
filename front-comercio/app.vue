05e69d5f7a3d80c10aecbe98fe359347fea753a5
BilbaoJ <jessicabilbao02@gmail.com>
2023-09-18 22:59:37 -0500

    Componente producto

--- front-comercio/app.vue
+++ front-comercio/app.vue
@@ -1,7 +1,18 @@
 <template>
-  <div>
-    <h1 class="text-xl">APP COMERCIO</h1>
-  </div>
+  <NuxtLayout :name="layout" class="">
+    <div class=" bg-slate-400 w-full h-fit flex justify-center">
+      <section class="bg-white min-h-screen h-fit w-5/6 my-10 p-10 grid grid-cols-5 auto-rows-auto gap-y-10">
+        <div v-for="producto in ComercioStore.productos" :key="producto.sku">
+          <Producto :producto="producto"/>
+        </div>
+        
+      </section>
+    </div>
+  </NuxtLayout>
 </template>
 <script setup lang="ts">
+import ComercioStore from "./store/ComercioStore";
+import Producto from "./components/Producto.vue"
+const layout = "comercio";
+
 </script>
---
+++ front-comercio/components/Producto.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="bg-white flex flex-col justify-center items-center">
+        <button @click="mostrarDetalle()">
+            <img src="../static/box.png" alt="Producto" class="w-40 h-40">
+            <h2 class="text-lg font-semibold text-black">{{ props.producto.nombre }}</h2>
+            <h1 class="text-xl font-semibold text-black">${{ props.producto.precio_unitario }} COP</h1>
+        </button>
+        <DetalleProducto v-if="mostrarDetalle" :producto="producto" @close="ocultarDetalle()"/>
+    </div>
+</template>
+<script setup lang="ts">
+import DetalleProducto from './DetalleProducto.vue';
+const props = defineProps({
+  producto: { type: Object, required: true }
+});
+
+var detalle = false;
+
+function mostrarDetalle(){
+    detalle = true;
+};
+
+function ocultarDetalle(){
+    detalle = false;
+};
+
+</script>
